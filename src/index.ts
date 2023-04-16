54abc5cdd494344fcad859fd75ce54891af3a08a
Ben <Ben@mail>
2023-04-16 18:59:22 +0100

    uint8array

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,28 @@
-function replacer(key:string, value:any) {
+function replacer(key: string, value: any) {
     if (value instanceof Map) {
         return {
             dataType: 'Map',
             value: Array.from(value.entries()), // or with spread: value: [...value]
         };
-    } else {
+    }
+    else if (value instanceof Uint8Array) {
+        return {
+            dataType: 'Uint8Array',
+            value: Array.from(value), // or with spread: value: [...value]
+        };
+    }
+    else {
         return value;
     }
 }
 
-function reviver(key:string, value:any) {
+function reviver(key: string, value: any) {
     if (typeof value === 'object' && value !== null) {
         if (value.dataType === 'Map') {
             return new Map(value.value);
+        }
+        if (value.dataType === 'Uint8Array') {
+            return new Uint8Array(value.value);
         }
     }
     return value;
@@ -24,3 +34,8 @@ export function stringify(value: any) {
 export function parse(text: string) {
     return JSON.parse(text, reviver)
 }
+
+// console.log("hi")
+// const x:Uint8Array = new Uint8Array([1,2,3])
+// console.log(x)
+// console.log(parse(stringify(x)))
